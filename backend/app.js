9687997964aaafe82d80cc6cf571587b64b666b5
ViniciusPolo <admviniciuspolo@gmail.com>
2023-05-01 21:21:37 -0300

    alteração total app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,10 @@
-const express = require("express")
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
 const cors = require('cors')
 const http = require('http')
 
 require('./database/indexDB')
-
-const dbConnection = require('./config/database')
-const app = express()
-dbConnection()
-
-app.use(express.json())
-app.use(cors())
 
 //Todas Rotas deverão ser descriminadas aqui
 const usersRoutes = require('./api/routes/usersRoutes')
@@ -17,25 +12,51 @@ const restaurantsRoutes = require('./api/routes/restaurantsRoutes')
 const menusRoutes = require('./api/routes/menusRoutes')
 const requestsRoutes = require('./api/routes/requestsRoutes')
 
+// const dbConnection = require('./config/database')
+// const app = express()
+// dbConnection()
+
+// Conecta ao banco de dados
+const dbConn = require('./config/database')
+
+const expressSession = require('express-session')
+const pgSession = require('connect-pg-simple')(expressSession)
+
+var app = express();
+
+const sessionConfig = {
+  store: new pgSession({
+    pool: dbConn
+  })
+}
+
+app.use(expressSession(sessionConfig))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 //E usadas aqui
 app.use(usersRoutes)
 app.use(restaurantsRoutes)
 app.use(menusRoutes)
 app.use('/requests',requestsRoutes)
 
-//Quando for fazer o deploy, colocar o que aqui?
-//app.set('url', 'http://localhost:');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-//app.set('port', 8443);
-// app.set('port', 3001);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-try {
-    http.createServer(app).listen(app.get('port'), function(){
-        console.log('Server started on port')
-        //console.log('Server started on '+ app.get('url') + app.get('port'))
-    })
-} catch (error) {
-    console.error("error deploy",error)
-}
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
