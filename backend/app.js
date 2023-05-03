321e53869f9b598be5fc1bf44dc4a714b734a593
ViniciusPolo <admviniciuspolo@gmail.com>
2023-05-02 21:35:29 -0300

    alteração package

--- backend/app.js
+++ backend/app.js
@@ -6,32 +6,30 @@ require('./database/indexDB')
 
 const dbConnection = require('./config/database')
 const app = express()
-//dbConnection()
+dbConnection()
 
 app.use(express.json())
 app.use(cors())
 
 //Todas Rotas deverão ser descriminadas aqui
 const usersRoutes = require('./api/routes/usersRoutes')
-// const restaurantsRoutes = require('./api/routes/restaurantsRoutes')
-// const menusRoutes = require('./api/routes/menusRoutes')
-// const requestsRoutes = require('./api/routes/requestsRoutes')
+const restaurantsRoutes = require('./api/routes/restaurantsRoutes')
+const menusRoutes = require('./api/routes/menusRoutes')
+const requestsRoutes = require('./api/routes/requestsRoutes')
 
 //E usadas aqui
 app.use(usersRoutes)
-// app.use(restaurantsRoutes)
-// app.use(menusRoutes)
-// app.use('/requests',requestsRoutes)
+app.use(restaurantsRoutes)
+app.use(menusRoutes)
+app.use('/requests',requestsRoutes)
 
 //Quando for fazer o deploy, colocar o que aqui?
-//app.set('url', 'http://localhost:');
-
-//app.set('port', 443);
+app.set('url', 'http://localhost:');
+app.set('port', 8080);
 // app.set('port', 3001);
 
 http.createServer(app).listen(app.get('port'), function(){
-    //console.log('Server started on '+ app.get('url') + app.get('port'))
-    console.log('Server started')
+    console.log('Server started on '+ app.get('url') + app.get('port'))
 })
 
 module.exports = app
