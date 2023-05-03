9db6d4656149faeb96d136b95d1c16d84302111b
ViniciusPolo <admviniciuspolo@gmail.com>
2023-05-03 14:24:08 -0300

    using pg-session attempt alter pool

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,47 @@
 require('dotenv').config()
-module.exports = {
-	host: process.env.HOST,
+
+// module.exports = {
+// 	host: process.env.HOST,
+// 	dialect: process.env.DIALECT,
+// 	username: process.env.USERNAME,
+// 	password: process.env.PASSWORD,
+//   port: process.env.PORT,
+//   database: process.env.DATABASE,
+//   logging: console.log,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//   },
+//   dialectOptions: {
+//     rejectUnauthorized: false,
+//     keepAlive: true,
+//     keepAliveInterval: 30000, // Tempo limite em milissegundos
+//   }
+// }
+
+const { Pool } = require('pg')
+
+const conn = new Pool({
+  host: process.env.HOST,
 	dialect: process.env.DIALECT,
 	username: process.env.USERNAME,
 	password: process.env.PASSWORD,
   port: process.env.PORT,
   database: process.env.DATABASE,
-  logging: console.log,
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-  dialectOptions: {
-    rejectUnauthorized: false,
-    keepAlive: true,
-    keepAliveInterval: 30000, // Tempo limite em milissegundos
+})
+
+// Testando a conexão
+async function testConn() {
+  try {
+    await conn.query('select now()')
+    console.log('** POSTGRES: conexão estabelecida')
+  }
+  catch(error) {
+    console.error('** POSTGRES: ERRO => ' + error)
   }
 }
+
+testConn()
+
+module.exports = conn
 
