8845f42ce31c762ebfcd28039ae57139b440633e
alandrade2 <alandrade.andre@gmail.com>
2023-08-19 15:55:02 -0300

    Atualizaçao pagina inicial

--- frontendWeb/src/pages/home/index.jsx
+++ frontendWeb/src/pages/home/index.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import "./style.css";
 
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 import Logo from "../../assets/logo.svg";
+import Rest from "../../assets/rest.svg";
+import Market from "../../assets/super.svg";
 
 export default function Home() {
   return (
@@ -33,14 +40,68 @@ export default function Home() {
         </header>
         <main>
           <div className="pesq">
-            <h1>Aqui tem um treim procê cumê</h1>
-            <p>É só procurar uai. Que nois entrega.</p>
+            <h2>Procurando algo pra comer?</h2>
+            <p>Peça aqui. Receba em casa.</p>
             <div className="proc">
               <input type="text" name="proc" id="" className="inpText" />
               <input type="button" value="Buscar" className="inpButton" />
             </div>
+            <div className="labels">
+              <div className="label1">
+                <h2>Restaurantes</h2>
+                <div className="merc">
+                  <p>
+                    Opções <span></span>
+                  </p>
+                  <img src={Rest} alt="" />
+                </div>
+              </div>
+              <div className="label1">
+                <h2>Mercados</h2>
+                <div className="merc">
+                  <p>
+                    Opções <span></span>
+                  </p>
+                  <img src={Market} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="linha-horizontal"></div>
+          <div className="listarest">
+            <h2>Restaurantes em destaque</h2>
           </div>
         </main>
+        <footer>
+          <ul>
+            <li>
+              <Link to="#">Fale conosco</Link>
+            </li>
+            <li>
+              <Link to="#">Privacidade</Link>
+            </li>
+            <li>
+              <Link to="#">Help-Desk</Link>
+            </li>
+          </ul>
+          <div className="redes">
+            <h2>Redes Sociais</h2>
+            <div className="icons">
+              <Link to="#">
+                <FaFacebook color="#fff" />
+              </Link>
+              <Link to="#">
+                <FaTwitter color="#fff" />
+              </Link>
+              <Link to="#">
+                <FaInstagram color="#fff" />
+              </Link>
+              <Link to="#">
+                <FaYoutube color="#fff" />
+              </Link>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
